f3df050d2826c0d260a34efdbc9c3f0d613deae2
Ramon Domingos <ramongemio@gmail.com>
2019-08-20 21:27:24 -0300

    adicao de campos com md5 e sha1

--- aula3/cliente.php
+++ aula3/cliente.php
@@ -3,6 +3,8 @@
         <title>Response </title>
     </head>
 <?php
+$var1 = 'Cliente';
+echo "<h1> Lista de ".$var1."</h1>";
 echo  "<h1>Escrever um conteudo no HTML  do reponse</h1>";
 echo "<h2>Dados do Cliente</h2>";
 echo "<b>Nome: </b>".filter_input(INPUT_POST,'nomecliente')."<br>"; 
@@ -12,5 +14,9 @@ echo "<b>Telefone: </b>".filter_input(INPUT_POST,'tel')."<br>";
 echo "<b>Idade: </b>".filter_input(INPUT_POST,'idade')."<br>"; 
 echo "<b>Id: </b>".filter_input(INPUT_POST,'id')."<br>"; 
 echo "<b>Sexo: </b>".filter_input(INPUT_POST,'sexo')."<br>"; 
+echo "<b>Senha: </b>".filter_input(INPUT_POST,'senha')."<br>"; 
+echo "<b>MD5: </b>".MD5(filter_input(INPUT_POST,'senha'))."<br>"; 
+echo "<b>SHA1: </b>".sha1(filter_input(INPUT_POST,'senha'))."<br>"; 
+echo "<b>8 DIGITOS: </b>".substr(MD5(filter_input(INPUT_POST,'senha')),0,8)."<br>"; 
 ?>
 </html>
