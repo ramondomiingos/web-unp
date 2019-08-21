67881f468dab4626d94d9d8d6fe19a92a5d2f68a
Ramon Domingos <ramongemio@gmail.com>
2019-08-20 21:20:10 -0300

    alteracao para post

--- aula3/cliente.php
+++ aula3/cliente.php
@@ -5,12 +5,12 @@
 <?php
 echo  "<h1>Escrever um conteudo no HTML  do reponse</h1>";
 echo "<h2>Dados do Cliente</h2>";
-echo "<b>Nome: </b>".filter_input(INPUT_GET,'nomecliente')."<br>"; 
-echo "<b>Data de Nascimento: </b>".filter_input(INPUT_GET,'dtnasc')."<br>"; 
-echo "<b>Email: </b>".filter_input(INPUT_GET,'email')."<br>"; 
-echo "<b>Telefone: </b>".filter_input(INPUT_GET,'tel')."<br>"; 
-echo "<b>Idade: </b>".filter_input(INPUT_GET,'idade')."<br>"; 
-echo "<b>Id: </b>".filter_input(INPUT_GET,'id')."<br>"; 
-echo "<b>Sexo: </b>".filter_input(INPUT_GET,'sexo')."<br>"; 
+echo "<b>Nome: </b>".filter_input(INPUT_POST,'nomecliente')."<br>"; 
+echo "<b>Data de Nascimento: </b>".filter_input(INPUT_POST,'dtnasc')."<br>"; 
+echo "<b>Email: </b>".filter_input(INPUT_POST,'email')."<br>"; 
+echo "<b>Telefone: </b>".filter_input(INPUT_POST,'tel')."<br>"; 
+echo "<b>Idade: </b>".filter_input(INPUT_POST,'idade')."<br>"; 
+echo "<b>Id: </b>".filter_input(INPUT_POST,'id')."<br>"; 
+echo "<b>Sexo: </b>".filter_input(INPUT_POST,'sexo')."<br>"; 
 ?>
 </html>
