453254724846b67904199a9932a80bcd48041e3b
Ramon Domingos <ramongemio@gmail.com>
2019-09-17 20:54:05 -0300

    adicionar funcoes matematicas para a calculadora

--- aulaJS/meusescripts.js
+++ aulaJS/meusescripts.js
@@ -1,6 +1,24 @@
 function somar(){
     var valor1 = document.getElementById("v1").value;
     var valor2 = document.getElementById("v2").value;
-    var res = number(valor1)+number(valor2);
+    var res = parseFloat(valor1)+parseFloat(valor2);
+    document.getElementById('numero').innerHTML = res;
+}
+function subtrair(){
+    var valor1 = document.getElementById("v1").value;
+    var valor2 = document.getElementById("v2").value;
+    var res = valor1-valor2;
+    document.getElementById('numero').innerHTML = res;
+}
+function dividir(){
+    var valor1 = document.getElementById("v1").value;
+    var valor2 = document.getElementById("v2").value;
+    var res = parseFloat(valor1)/parseFloat(valor2);
+    document.getElementById('numero').innerHTML = res;
+}
+function multiplicar(){
+    var valor1 = document.getElementById("v1").value;
+    var valor2 = document.getElementById("v2").value;
+    var res = parseFloat(valor1)*parseFloat(valor2);
     document.getElementById('numero').innerHTML = res;
 }
