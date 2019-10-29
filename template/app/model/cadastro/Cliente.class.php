96ecadd66c585585d9ffd737044e21074b7053bc
Ramon Domingos <ramongemio@gmail.com>
2019-10-29 20:46:33 -0300

    arquivos do professor, continuacao

--- template/app/model/cadastro/Cliente.class.php
+++ template/app/model/cadastro/Cliente.class.php
@@ -5,9 +5,9 @@
  * @version    1.0
  * @package    model
  * @subpackage admin
- * @author     Ramon
- * @copyright  Copyright (c) 2006 Adianti Solutions Ltd. (http://www.adianti.com.br)
- * @license    http://www.adianti.com.br/framework-license
+ * @author     Eu o Aluno
+ * @copyright  Copyright (c) 2019 UnP. (http://www.unp.br)
+ * @license    http://www.unp.br/
  */
 class Cliente extends TRecord
 {
@@ -15,15 +15,24 @@ class Cliente extends TRecord
     const PRIMARYKEY= 'id';
     const IDPOLICY =  'serial'; // {max, serial}
     
-    
     /**
      * Constructor method
      */
     public function __construct($id = NULL, $callObjectLoad = TRUE)
     {
         parent::__construct($id, $callObjectLoad);
-        parent::addAttribute('nome');
-        parent::addAttribute('datanascimento');
-
+        parent::addAttribute('nomecliente');
+        parent::addAttribute('tipocliente_id');
+        parent::addAttribute('dtnasc');
+        parent::addAttribute('cpf');
+        parent::addAttribute('endereco');
+        parent::addAttribute('bairro');
+        parent::addAttribute('cidade');
+        parent::addAttribute('cep');
+        parent::addAttribute('tel');
+        parent::addAttribute('cel');
+        parent::addAttribute('email');
+        parent::addAttribute('sexo');
+        parent::addAttribute('limite');
     }
 }
